72ca09315216df63bc78e51c457572d86ac21700
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2022-03-09 14:28:39 -0300

    opa

--- dump_postgresql.sh
+++ dump_postgresql.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-USERNAME = "postgres"
+USERNAME="postgres"
 PASSWORD="postgres"
 HOST="localhost"
-PORT = "5432"
-DBNAME = "teste"
-FILE_NAME = "teste.sql"
-PGPASSWORD=$PASSWORD pg_dump --host $HOST --port $PORT --username $USERNAME --format plain --create --clean --inserts --verbose --file $FILE_NAME $DBNAME
+PORT="5432"
+DBNAME="teste"
+FILE_NAME="teste.sql"
+
+COMMAND="PGPASSWORD=${PASSWORD} pg_dump --host ${HOST} --port ${PORT} --username ${USERNAME} --format plain --create --clean --inserts --verbose --file ${FILE_NAME} ${DBNAME}"
+eval $COMMAND
