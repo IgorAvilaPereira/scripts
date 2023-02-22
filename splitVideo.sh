29588c9e5433aa54a46b58739bf05471fdc7954b
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-02-22 13:57:38 -0300

    split video

---
+++ splitVideo.sh
@@ -0,0 +1,4 @@
+# sudo apt install gpac
+
+# exemplo (quebra o video MAH00124.mp4 em partes de 15 segundos)
+MP4Box -split 15 /home/asafche/Videos/myVideos/MAH00124.MP4
--- troca_palavras_de_um_diretorio.sh
+++ troca_palavras_de_um_diretorio.sh
@@ -1,4 +1,4 @@
-find ./ -type f -exec sed -i 's/pg_query/$conexao->query/gI' {} \;
-find ./ -type f -exec sed -i 's/pg_fetch_array($result)/$resultado->fetchArray()/gI' {} \;
+find ./ -type f -exec sed -i 's/logo_ifrs/logo/gI' {} \;
+find ./ -type f -exec sed -i 's/igor/teste/gI' {} \;
 
 
