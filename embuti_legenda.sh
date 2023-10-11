342393bf771ca2636047778354669f1b8d1ad352
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-10-11 17:00:36 -0300

    Update embuti_legenda.sh

--- embuti_legenda.sh
+++ embuti_legenda.sh
@@ -1,11 +1,3 @@
-mkvmerge -o Twisted.Metal.S01E01.1080p.WEB.h264-EDITH_with_subs.mkv Twisted.Metal.S01E01.1080p.WEB.h264-EDITH.mkv Twisted.Metal.S01E01.1080p.WEB.h264-EDITH.srt
-mkvmerge -o Twisted.Metal.S01E02.1080p.WEB.h264-EDITH_with_subs.mkv Twisted.Metal.S01E02.1080p.WEB.h264-EDITH.mkv Twisted.Metal.S01E02.1080p.WEB.h264-EDITH.srt
+# sudo apt install mkvtoolnix
 
-# mkvmerge -o Twisted.Metal.S01E03.1080p.WEB.h264-EDITH_with_subs.mkv Twisted.Metal.S01E03.1080p.WEB.h264-EDITH.mkv Twisted.Metal.S01E03.1080p.WEB.h264-EDITH.srt
-# mkvmerge -o Twisted.Metal.S01E04.1080p.WEB.h264-EDITH_with_subs.mkv Twisted.Metal.S01E04.1080p.WEB.h264-EDITH.mkv Twisted.Metal.S01E04.1080p.WEB.h264-EDITH.srt
-# mkvmerge -o Twisted.Metal.S01E05.1080p.WEB.h264-EDITH_with_subs.mkv Twisted.Metal.S01E05.1080p.WEB.h264-EDITH.mkv Twisted.Metal.S01E05.1080p.WEB.h264-EDITH.srt
-# mkvmerge -o Twisted.Metal.S01E06.1080p.WEB.h264-EDITH_with_subs.mkv Twisted.Metal.S01E06.1080p.WEB.h264-EDITH.mkv Twisted.Metal.S01E06.1080p.WEB.h264-EDITH.srt
-# mkvmerge -o Twisted.Metal.S01E07.1080p.WEB.h264-EDITH_with_subs.mkv Twisted.Metal.S01E07.1080p.WEB.h264-EDITH.mkv Twisted.Metal.S01E07.1080p.WEB.h264-EDITH.srt
-# mkvmerge -o Twisted.Metal.S01E08.1080p.WEB.h264-EDITH_with_subs.mkv Twisted.Metal.S01E08.1080p.WEB.h264-EDITH.mkv Twisted.Metal.S01E08.1080p.WEB.h264-EDITH.srt
-# mkvmerge -o Twisted.Metal.S01E09.1080p.WEB.h264-EDITH_with_subs.mkv Twisted.Metal.S01E09.1080p.WEB.h264-EDITH.mkv Twisted.Metal.S01E09.1080p.WEB.h264-EDITH.srt
-# mkvmerge -o Twisted.Metal.S01E10.1080p.WEB.h264-EDITH_with_subs.mkv Twisted.Metal.S01E10.1080p.WEB.h264-EDITH.mkv Twisted.Metal.S01E10.1080p.WEB.h264-EDITH.srt
+mkvmerge -o video_output_with_subtitle.mkv video_input.mkv subtitle.srt
