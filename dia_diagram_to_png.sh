6c68c5fbaa8ee3167b8e03759ac35a40d8cce467
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2025-12-01 19:48:07 -0300

    ok

--- dia_diagram_to_png.sh
+++ dia_diagram_to_png.sh
@@ -1,1 +1,7 @@
-for D in *.dia; do dia -t png -s 1024x $D; done
+USER_PATH=$(zenity --entry --title="Dia to PNG" --text="Please enter your path:")
+if [ -n "$USER_PATH" ]; then
+    cd $USER_PATH
+    for D in *.dia; do dia -t png -s 1024x $D; done
+else
+    echo "No path entered."
+fi
---
+++ pdfannots-app.sh
@@ -0,0 +1,2 @@
+zenity --info --title="Pdfannots" --text="Carregando..."
+cd /home/iapereira/git/pdfannots-app/src && flet run
