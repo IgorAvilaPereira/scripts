03a2e7999c1763b7a41d7f72487a7be2ee689995
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2025-12-01 19:52:39 -0300

    ok

--- plantuml_exemplo.sh
+++ plantuml_exemplo.sh
@@ -1,3 +1,6 @@
-iapereira@vostro3360:~$ plantuml teste.uml 
-iapereira@vostro3360:~$ plantuml -tsvg teste.uml 
-
+USER_NAME=$(zenity --entry --title="PlantUML to SVG" --text="Please enter your path+plantfile:")
+if [ -n "$USER_NAME" ]; then
+    plantuml -tsvg $USER_NAME
+else
+    echo "No name entered."
+fi
